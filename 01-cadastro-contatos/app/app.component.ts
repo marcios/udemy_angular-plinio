b16b9972e390c3d8d0c982aa06e946f6a91b9e75
marcios <marcioj.fsantos@outlook.com>
2018-01-29 23:57:11 -0200

    first

---
+++ 01-cadastro-contatos/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector:'my-app',
+	template:`<h1>My First angular 2 App</h1>
+
+	<contatos-lista></contatos-lista>
+
+	`
+})
+export class AppComponent{
+
+}
---
+++ 01-cadastro-contatos/app/contatos/contatos-lista.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from '@angular/core'
+
+@Component({
+	moduleId:module.id,
+	selector:'contatos-lista',
+	templateUrl:'contatos-lista.component.html',
+
+})
+export class ContatosListaComponent{
+
+}
